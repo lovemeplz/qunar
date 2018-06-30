a6dbaa3967d635329b7fc07336b031b11da8e1e9
lovemeplz <1143783926@qq.com>
2018-06-30 12:41:13 +0800

    add city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
-<div class="header">
-  <div class="header-left">
-    <div class="iconfont back-icon">&#xe624;</div>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+        北京
+      </div>
+    </router-link>
   </div>
-  <div class="header-input">
-    <span class="iconfont">&#xe632;</span>
-    输入城市/景点/游玩主题
-  </div>
-  <div class="header-right">
-    <span class="iconfont arrow-icon">&#xe64a;</span>
-    北京
-  </div>
-</div>
 </template>
 <script>
 export default {
