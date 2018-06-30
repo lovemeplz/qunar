587690476267fadca96c76163bcc12074dc424ca
lovemeplz <1143783926@qq.com>
2018-06-30 11:32:46 +0800

    add index-recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,21 +4,24 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
-import HomeSwiper from './components/swiper'
-import HomeIcons from './components/icons'
-import HomeRecommend from './components/recommend'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
