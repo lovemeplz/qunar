811553662e65aec21e45a84136d258336e2a5ca8
lovemeplz <1143783926@qq.com>
2018-06-30 12:06:19 +0800

    add index-index

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
-  <swiper :options='swiperOption'>
+  <swiper :options='swiperOption' v-if='swiperList.length'>
       <swiper-slide
-        v-for='item of imgs'
+        v-for='item of swiperList'
         :key='item.id'
       >
         <img :src="item.imgUrl" class='swiper-img'>
@@ -15,26 +15,16 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    swiperList: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true,
         autoplay: 3000
-      },
-      imgs: [{
-        'id': '0001',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'
-      }, {
-        'id': '0002',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
-      }, {
-        'id': '0003',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
-      }, {
-        'id': '0004',
-        'imgUrl': 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
-      }]
+      }
     }
   }
 }
