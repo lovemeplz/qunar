874e8c74310dfd4611f37426cff22d7bfb600efb
lovemeplz <1143783926@qq.com>
2018-07-03 23:25:28 +0800

    search

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <ul class="list" v-if="keyword">
+      <li
+       class="item border-bottom"
+       v-for="item in list"
+       :key="item.id"
+       v-if="list.length"
+      >{{item.name}}
+      </li>
+      <li class="item border-bottom">没有找到匹配数据</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: []
+    }
+  },
+  mounted () {
+    console.log('初始值:' + this.list.length)
+  },
+  watch: {
+    keyword () {
+      const result = []
+      for (let i in this.cities) {
+        this.cities[i].forEach((val) => {
+          if (this.keyword && (val.spell.indexOf(this.keyword) > -1 ||
+            val.name.indexOf(this.keyword) > -1)) {
+            result.push(val)
+          }
+        })
+      }
+      this.list = result
+      console.log(this.list.length)
+    }
+  }
 }
 </script>
 
@@ -24,4 +63,16 @@ export default {
       box-sizing:border-box
       color:#666
       border-radius:.06rem
+  .list
+    position:absolute
+    top:1.58rem
+    left:0
+    right:0
+    bottom:0
+    background:#eee
+    z-index:10
+    .item
+      padding-left:.2rem
+      line-height:.76rem
+      background:#fff
 </style>
